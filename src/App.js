45b2f0a6ce9ea954e933ef701c4c2c52dbb918d3
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-06-25 20:48:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CarContainer from "./components/CarContainer";
+import NavBar from "./components/Navbar";
+
+function App() {
+  return(
+    <main>
+      <NavBar/>
+      <CarContainer/>
+    </main>
+  ) 
+}
+export default App;
