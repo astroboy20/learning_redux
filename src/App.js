5dd798b3bf445029b4e563764a3cba80e7a757f6
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-06-25 23:31:19 +0100

    final part of redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import CarContainer from "./components/CarContainer";
 import NavBar from "./components/Navbar";
-
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals,getCartItems } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
 function App() {
-  return(
+  const { cartItems,isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if(isLoading){
+    return <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  }
+
+  return (
     <main>
-      <NavBar/>
-      <CarContainer/>
+      {isOpen && <Modal />}
+      <NavBar />
+      <CarContainer />
     </main>
-  ) 
+  );
 }
 export default App;
--- src/components/CarContainer.js
+++ src/components/CarContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import CartItem from "./CartItem";
 import {useDispatch, useSelector } from "react-redux";
 import { clearCart } from "../features/cart/cartSlice";
+import { openModal } from "../features/modal/modalSlice";
 
 const CarContainer = () => {
   const {cartItems, total, amount } = useSelector((store) => store.cart);
@@ -29,9 +30,9 @@ const dispatch = useDispatch()
         <footer>
             <hr/>
             <div className="cart-total">
-            <h4>total <span>$ {total}</span></h4>
+            <h4>total <span>$ {total.toFixed(2)}</span></h4>
             </div>
-            <button onClick={()=>dispatch(clearCart())}  className="btn clear-btn">clear cart</button>
+            <button onClick={()=>dispatch(openModal())}  className="btn clear-btn">clear cart</button>
         </footer>
     </section>
   ) 
